c4ddbc1e04d26b288d5a2ac3b654da7c9e55d793
Dikshyant <dhunganadikshyant12@gmail.com>
2022-11-20 17:13:31 +0545

    generator functions created

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
-const resultEl = document.getElementById(result);
-const upperEl = document.getElementById(upper);
-const lowerEl = document.getElementById(lower);
-const numberEl = document.getElementById(numbers);
-const symbolEl = document.getElementById(symbols);
-const lengthEl = document.getElementById(length);
-const generateEl = document.getElementById(generate);
-const clipboardEl = document.getElementById(clipboard);
+// DOM Elements
+const resultEl = document.getElementById("result");
+const upperEl = document.getElementById("upper");
+const lowerEl = document.getElementById("lower");
+const numberEl = document.getElementById("numbers");
+const symbolEl = document.getElementById("symbols");
+const lengthEl = document.getElementById("length");
+const generateEl = document.getElementById("generate");
+const clipboardEl = document.getElementById("clipboard");
 
+generate.addEventListener("click", () => {
+  const length = +lengthEl.value;
+  const hasUpper = upperEl.checked;
+  const hasLower = lowerEl.checked;
+  const hasNumber = numberEl.checked;
+  const hasSymbol = symbolEl.checked;
+  //   const hasUpper=upperEl.checked;
+  //   console.log(hasLower);
+  resultEl.innerText = generatePassword(
+    hasUpper,
+    hasLower,
+    hasNumber,
+    hasSymbol
+  );
+});
+
+// Generator functions
 function getRandomUpper() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
